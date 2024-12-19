b39253a8fdce2164056a9eebdd1b9e61b60d897a
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2024-12-19 18:32:58 +0600

    reading entire file

--- backend/src/main.c
+++ backend/src/main.c
@@ -1,7 +1,47 @@
 #include "imlib/imlog.h"
+#include "imlib/impanic.h"
+#include "imlib/imstdinc.h"
+#include "imlib/imerrno.h"
 
-int main(void) {
-  imlog(LOG_INFO, "Hello world");
-  return 0;
+#include <sqlite3.h>
+#include <stdlib.h>
+
+PRIVATE size_t SizeOfFile(register FILE *const fp) {
+  register long size = 0u;
+
+  NEQ(fseek(fp, 0, SEEK_END), -1);
+  NEQ(size = ftell(fp), -1);
+  NEQ(fseek(fp, 0, SEEK_SET), -1);
+
+  return (size_t) size;
+}
+
+PRIVATE char *ReadEntireFile(register char const *const file) {
+  register FILE *fp = NULL;
+  register char *content = NULL;
+  register size_t size = 0;
+
+  NEQ(fp = fopen(file, "r"), NULL);
+  size = SizeOfFile(fp);
+  NEQ(content = calloc(1u + size, sizeof(*content)), NULL);
+  NEQ(fread(content, sizeof(*content), size, fp), 0);
+  EQ(fclose(fp), 0);
+
+  return content;
+}
+
+PRIVATE void start(void) {
+  register char *const content = ReadEntireFile("database/add_user.sql");
+  (void)printf("%s\n", content);
+  free(content);
+}
+
+
+PUBLIC int main(register int const argc, register char const *const *const argv) {
+  (void)argc;
+  trace_target = argv[0];
+  imclrerr();
+  start();
+  return EXIT_SUCCESS;
 }
 
